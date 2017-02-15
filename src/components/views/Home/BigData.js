79a5e62e42b139495e84a7093f435cb7c191e15a
Joon Park <parkjoon@umich.edu>
2017-02-15 16:15:32 -0500

    Fixed the BigData section.

--- src/components/views/Home/BigData.js
+++ src/components/views/Home/BigData.js
@@ -3,41 +3,16 @@ import React, { Component } from 'react';
 export default class BigData extends Component {
 	render() {
 		return (
-			// <section className="section-90 section-md-110 inset-left-15 inset-right-15">
-			// 	<div className="row">
-			// 		<div className="col-md-12">
-			// 			<div className="row">
-			// 				<div className="col-md-12">
-			// 					<h3>Big Data (THIS SECTION IS A WORK IN PROGRESS)</h3>
-			// 				</div>
-			// 			</div>
-			// 			<div className="row" style={{marginTop: '1em'}}>
-			// 				<div className="col-md-6" style={{marginLeft: '25%'}}>
-			// 					<p>
-			// 						By leveraging our knowledge of Data Warehousing and Business Intelligence and by drawing standard reports, we help businesses view their existing data in a more efficient way.
-			// 					</p>
-			// 				</div>
-			// 			</div>
-			// 			<div className="row" style={{marginTop: '1em'}}>
-			// 				<div className="col-md-6" style={{marginLeft: '25%'}}>
-			// 					<p>
-			// 						Our data mining capabilities have helped businesses unearth a plethora of previously unseen information. Our expertise in Big Data have enabled our clients efficiently handle unstructured and semi structured data efficiently, thereby lowering their operational costs. Finally, our proficiency in Predictive Analytics and Machine Learning have helped companies forecast their foreseeable future and modify their business decisions.
-			// 					</p>
-			// 				</div>
-			// 			</div>
-			// 		</div>
-			// 	</div>
-			// </section>
 			<section className="context-dark">
 				<div data-on="false" data-md-on="true" className="rd-parallax">
 					<div data-speed="0.35" data-type="media" data-url="/styles/images/tronix-data/banner-6.jpg" className="rd-parallax-layer"></div>
 					<div data-speed="0" data-type="html" className="rd-parallax-layer">
 						<div className="shell">
 							<div className="range">
-								<div className="range range-xs-center range-md-left range-xs-middle text-md-left" style={{minHeight: '500px'}}>
-									<div className="cell-xs-10 cell-md-8">
-										<h1 style={{pointerEvents: 'none', paddingTop: '1em'}}>Big Data (THIS SECTION IS A WORK IN PROGRESS)</h1>
-										<p style={{pointerEvents: 'none'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								<div className="range range-xs-center range-xs-center">
+									<div className="cell-xs-10 cell-md-8" style={{marginBottom: '5em'}}>
+										<h1 style={{pointerEvents: 'none', paddingTop: '1em'}}>Big Data</h1>
+										<p style={{pointerEvents: 'none'}}>The answer to most of a company’s questions can be found in their data. With the right approach, right tools and the right people you can rectify the past, assess the present and not just predict the future, but also possess the ability change it.<br /><br />For companies that need to make sense out of their Big Data, Tronix Data provides Solutions, Expertise, and Training to take on their Big Data challenges. We are tried and tested Big Data industry veterans with years of direct use case experiences and can tailor our services to your exact requirements.<br /><br />We have the expertise, the man power, the experiences and the market understanding to fully enable Big Data Technologies for the benefit of businesses and individuals alike.</p>
 									</div>
 								</div>
 							</div>
--- src/components/views/Home/MiscFeatures.js
+++ src/components/views/Home/MiscFeatures.js
@@ -3,41 +3,20 @@ import React, { Component } from 'react';
 export default class MiscFeatures extends Component {
 	render() {
 		return (
-			<section className="section-90 section-md-110 inset-left-15 inset-right-15" style={{paddingBottom: '3em', paddingTop: '3em'}}>
+			<section className="section-90 section-md-110 inset-left-15 inset-right-15" style={{paddingBottom: '6em', paddingTop: '3em', marginLeft: '1em', marginRight: '1em'}}>
 				<div className="row">
 					<div className="range range-xs-center offset-top-30">
 						<div className="cell-xs-11 cell-md-9">
 							<h3>What We Do</h3>
+							<p style={{color: 'black'}}>
+								Our vision is to help organizations succeed through a combination of machine learning and big data analytics while innovating and delivering products that would make decision making simpler for enterprises and small businesses alike.
+							</p>
 							<p style={{color: 'black'}}>
 								By leveraging our knowledge of Data Warehousing and Business Intelligence and by drawing standard reports, we help businesses view their existing data in a more efficient way.
 							</p>
 							<p style={{color: 'black'}}>
 								Our data mining capabilities have helped businesses unearth a plethora of previously unseen information. Our expertise in Big Data have enabled our clients efficiently handle unstructured and semi structured data efficiently, thereby lowering their operational costs. Finally, our proficiency in Predictive Analytics and Machine Learning have helped companies forecast their foreseeable future and modify their business decisions.
 							</p>
-						</div>
-					</div>
-				</div>
-				<div className="row" style={{marginTop: '5em'}}>
-					<div className="col-md-12">
-						<div className="col-xs-3">
-							<div className="preview"><img src="/styles/images/tronix-data/data-management-1.png" width="370" height="240" className="img-responsive" />
-								<p className="preview-title">Data Management</p>
-							</div>
-						</div>
-						<div className="col-xs-3">
-							<div className="preview"><img src="/styles/images/tronix-data/iot-1.jpg" width="370" height="240" className="img-responsive" />
-								<p className="preview-title">Internet of Things</p>
-							</div>
-						</div>
-						<div className="col-xs-3">
-							<div className="preview"><img src="/styles/images/tronix-data/ai-1.jpg" width="370" height="240" className="img-responsive" />
-								<p className="preview-title">Machine Learning</p>
-							</div>
-						</div>
-						<div className="col-xs-3">
-							<div className="preview"><img src="/styles/images/tronix-data/data-vis-1.jpg" width="370" height="240" className="img-responsive" />
-								<p className="preview-title">Data Visualization</p>
-							</div>
 						</div>
 					</div>
 				</div>
