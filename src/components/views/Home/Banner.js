d7ca33a06049c27f0d0386e6727907522645e7d1
Joon Park <parkjoon@umich.edu>
2017-03-11 10:25:59 -0500

    P test

--- src/components/views/Home/Banner.js
+++ src/components/views/Home/Banner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Banner extends Component {
 	render() {
 		return (
-			<div style={{maxHeight: '1000px'}}>
+			<div>
 				<div className="banner-parallax">
 					<div className="shell">
 						<div className="range">
